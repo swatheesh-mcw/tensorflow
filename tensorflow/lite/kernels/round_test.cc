64372d51aa5e4b7a9bf280a6b80fa3bfebbb260e
Swatheesh Muralidharan <swatheesh.muralidharan@multicorewareinc.com>
2024-07-18 15:57:04 +0530

    Added float16 and bfloat16 support for tflite round operation

--- tensorflow/lite/kernels/round_test.cc
+++ tensorflow/lite/kernels/round_test.cc
@@ -13,10 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <gtest/gtest.h>
+
 #include <initializer_list>
 #include <vector>
 
-#include <gtest/gtest.h>
+#include "Eigen/Core"
 #include "tensorflow/lite/kernels/test_util.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
@@ -62,6 +64,71 @@ TEST(RoundOpTest, MultiDims) {
   ASSERT_EQ(model.Invoke(), kTfLiteOk);
   EXPECT_THAT(model.GetOutput(),
               ElementsAreArray({0, 8, 1, 10, 0, 0, -8, -1, -10, -0, -2, 2}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 1, 1, 6}));
+}
+
+TEST(RoundOpTest, Float16SingleDim) {
+  RoundOpModel<Eigen::half> model({6}, TensorType_FLOAT16);
+  model.PopulateTensor<Eigen::half>(
+      model.input(), {Eigen::half(8.5), Eigen::half(0.0), Eigen::half(3.5),
+                      Eigen::half(4.2), Eigen::half(-3.5), Eigen::half(-4.5)});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(
+      model.GetOutput(),
+      ElementsAreArray({Eigen::half(8), Eigen::half(0), Eigen::half(4),
+                        Eigen::half(4), Eigen::half(-4), Eigen::half(-4)}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({6}));
+}
+
+TEST(RoundOpTest, Float16MultiDims) {
+  RoundOpModel<Eigen::half> model({2, 1, 1, 6}, TensorType_FLOAT16);
+  model.PopulateTensor<Eigen::half>(
+      model.input(),
+      {Eigen::half(0.0001), Eigen::half(8.0001), Eigen::half(0.9999),
+       Eigen::half(9.9999), Eigen::half(0.5), Eigen::half(-0.0001),
+       Eigen::half(-8.0001), Eigen::half(-0.9999), Eigen::half(-9.9999),
+       Eigen::half(-0.5), Eigen::half(-2.5), Eigen::half(1.5)});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(
+      model.GetOutput(),
+      ElementsAreArray({Eigen::half(0), Eigen::half(8), Eigen::half(1),
+                        Eigen::half(10), Eigen::half(0), Eigen::half(0),
+                        Eigen::half(-8), Eigen::half(-1), Eigen::half(-10),
+                        Eigen::half(-0), Eigen::half(-2), Eigen::half(2)}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 1, 1, 6}));
+}
+
+TEST(RoundOpTest, BFloat16SingleDim) {
+  RoundOpModel<Eigen::bfloat16> model({6}, TensorType_BFLOAT16);
+  model.PopulateTensor<Eigen::bfloat16>(
+      model.input(),
+      {Eigen::bfloat16(8.5), Eigen::bfloat16(0.0), Eigen::bfloat16(3.5),
+       Eigen::bfloat16(4.2), Eigen::bfloat16(-3.5), Eigen::bfloat16(-4.5)});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(model.GetOutput(),
+              ElementsAreArray({Eigen::bfloat16(8), Eigen::bfloat16(0),
+                                Eigen::bfloat16(4), Eigen::bfloat16(4),
+                                Eigen::bfloat16(-4), Eigen::bfloat16(-4)}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({6}));
+}
+
+TEST(RoundOpTest, BFloat16MultiDims) {
+  RoundOpModel<Eigen::bfloat16> model({2, 1, 1, 6}, TensorType_BFLOAT16);
+  model.PopulateTensor<Eigen::bfloat16>(
+      model.input(),
+      {Eigen::bfloat16(0.0001), Eigen::bfloat16(8.0001),
+       Eigen::bfloat16(0.9999), Eigen::bfloat16(9.9999), Eigen::bfloat16(0.5),
+       Eigen::bfloat16(-0.0001), Eigen::bfloat16(-8.0001),
+       Eigen::bfloat16(-0.9999), Eigen::bfloat16(-9.9999),
+       Eigen::bfloat16(-0.5), Eigen::bfloat16(-2.5), Eigen::bfloat16(1.5)});
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  EXPECT_THAT(
+      model.GetOutput(),
+      ElementsAreArray(
+          {Eigen::bfloat16(0), Eigen::bfloat16(8), Eigen::bfloat16(1),
+           Eigen::bfloat16(10), Eigen::bfloat16(0), Eigen::bfloat16(0),
+           Eigen::bfloat16(-8), Eigen::bfloat16(-1), Eigen::bfloat16(-10),
+           Eigen::bfloat16(-0), Eigen::bfloat16(-2), Eigen::bfloat16(2)}));
   EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({2, 1, 1, 6}));
 }
 
